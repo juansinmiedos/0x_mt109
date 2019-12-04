4bd461e99d7bb339fd0469f0131b6f0c858ae2ae
Juan Vidal <juanvidaldom@gmail.com>
2019-12-04 08:21:51 -0600

    minor sintax mistake solved

--- diagonal-difference/index.js
+++ diagonal-difference/index.js
@@ -1,6 +1,6 @@
 const squareMatrix = [[1, 2, 3], [4, 5, 6], [9, 8, 9]]
 
-absoluteDifference = (arrOfarrs) => {
+const absoluteDifference = (arrOfarrs) => {
     const firstDiagonal = arrOfarrs.map((el, i) => el[i])
     const secondDiagonal = arrOfarrs.map((el, i, arr) => el[arr.length - i -1])
 
