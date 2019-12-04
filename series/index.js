be30bd888720b5f3719a078002c01339d8ce2198
Juan Vidal <juanvidaldom@gmail.com>
2019-12-04 08:20:38 -0600

    series solved

--- series/index.js
+++ series/index.js
@@ -1,11 +1,30 @@
-// const stringOfDigits = '49142'
+const stringOfDigits = '49142'
 
-// seriesOfDigits = (string, lengthOfEachSubstring) => {
-//     const stringToArray = string.split('')
-//     const arrOfCombinations = []
+const seriesOfDigits = (string, lengthOfEachSubstring) => {
+    const stringToArray = string.split('')
 
-//     return arrOfCombinations
-// }
+    if(stringToArray.length < lengthOfEachSubstring){
+        return "You deserve whatever you get!"
+    } else if(stringToArray.length === lengthOfEachSubstring){
+        return string
+    } else if(stringToArray.length > lengthOfEachSubstring){
+        const arrOfCombinations = stringToArray.map((el, id, arr)=> {
+            const aux = []
 
-// seriesOfDigits(stringOfDigits, 3)
-// seriesOfDigits(stringOfDigits, 4)
+            for(i = id; i < (lengthOfEachSubstring + id); i++){
+                aux.push(stringToArray[i])
+            }
+
+            return aux.join('')
+        })
+
+        for(i = 0; i < (lengthOfEachSubstring - 1); i++){
+            arrOfCombinations.pop()
+        }
+
+        return arrOfCombinations
+    }
+}
+
+seriesOfDigits(stringOfDigits, 3)
+seriesOfDigits(stringOfDigits, 4)
